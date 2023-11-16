74e894f3a78683ec098e5c4df67723ba4aba2878
akbar202e <akbar202e@gmail.com>
2023-11-16 10:04:09 +0700

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Tentang from './pages/Tentang'
+import NotFound from './pages/NotFound'
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage/>} />
+          <Route path="/about" element={<Tentang/>} />
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
