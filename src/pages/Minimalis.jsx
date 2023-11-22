49d588f1f3546139d78eacf443c5bc79109590f7
Your Name <you@example.com>
2023-11-22 10:23:32 +0700

    #

---
+++ src/pages/Minimalis.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import '../assets/css/Minimalis.jsx'
+
+function Minimalis() {
+  return (
+    <>
+    <NavbarC />
+    <div className="box">
+      <div className="group">
+        <div className="overlap-group">
+          <img className="arrow" alt="Arrow" src="arrow.png" />
+          <div className="rectangle" />
+          <div className="div" />
+          <div className="rectangle-2" />
+          <div className="text-wrapper">Pesan Sekarang !</div>
+          <img className="img" alt="Rectangle" src="rectangle-555.png" />
+          <img className="arrow-2" alt="Arrow" src="image.png" />
+          <img className="arrow-3" alt="Arrow" src="arrow-2.png" />
+          <div className="rectangle-3" />
+          <div className="text-wrapper-2">2 KM</div>
+          <p className="p">10 m x 20 m</p>
+          <div className="text-wrapper-3">Kota Semarang</div>
+          <img className="bed" alt="Bed" src="bed.png" />
+          <img className="size" alt="Size" src="size.png" />
+          <img className="location" alt="Location" src="location.png" />
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Minimalis
