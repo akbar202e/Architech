66ee7f9f5c207683d48155864d6b6dfa11f2250b
Your Name <you@example.com>
2023-11-23 12:36:54 +0700

    Add SliderComponen

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import Tentang from './pages/Tentang'
+import Portofolio from './pages/Portofolio'
+import Minimalis from './pages/Minimalis'
 import NotFound from './pages/NotFound'
+
+
 function App() {
   return (
     <div>
@@ -10,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage/>} />
           <Route path="/about" element={<Tentang/>} />
+          <Route path="/Portofolio" element={<Portofolio/>} />
+          <Route path="/Minimalis" element={<Minimalis/>} />
           <Route path="*" element={<NotFound/>} />
         </Routes>
       </Router>
--- src/pages/Minimalis.jsx
+++ src/pages/Minimalis.jsx
@@ -1,34 +1,37 @@
-import React from 'react'
+import HeroSlider from '../components/HeroSlider';
 
-import '../assets/css/Minimalis.jsx'
+import { Button } from 'react-bootstrap';
 
-function Minimalis() {
+import '../assets/css/Minimalis.css'
+import NavbarC from '../components/NavbarC';
+
+
+function Minimalis  () {
+  const slides = [
+    { url: "https://s3-alpha-sig.figma.com/img/69dc/f29a/d1619903a8100ba617d7880723e298d7?Expires=1701648000&Signature=VqGfuLFniJeUyvQYozMWlbzrE1eS0LWJOMDbMa4p6xRGPSSR8isAR45y0NJTJgy1lJXs60H1IIB6AN0wmIAHRm1yXQaqKnFbUkQN1i9JfitlYf2zCzpeyUXw29BjeLnuuSHGwNq4jae2ero6HJdBTYarj2WYIycZn3y79pdOahRyQYd5RNpWK2E~0bOKVO61uSorLWQNnItYWAfI2-eg~P1csr3TtaYW70YCxlIdPyTWBLEtR5ltOz1-phLCOs82wweKCOpL~RvEjTEIaG9wr~CDE2w5hOtF0lHb1IdnsMIcShkiodgTGgFmrf4OD4uoYX9nGrOqvebLfhZ1QmdNgw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4", title: "Minimalis" },
+    { url: "https://s3-alpha-sig.figma.com/img/69dc/f29a/d1619903a8100ba617d7880723e298d7?Expires=1701648000&Signature=VqGfuLFniJeUyvQYozMWlbzrE1eS0LWJOMDbMa4p6xRGPSSR8isAR45y0NJTJgy1lJXs60H1IIB6AN0wmIAHRm1yXQaqKnFbUkQN1i9JfitlYf2zCzpeyUXw29BjeLnuuSHGwNq4jae2ero6HJdBTYarj2WYIycZn3y79pdOahRyQYd5RNpWK2E~0bOKVO61uSorLWQNnItYWAfI2-eg~P1csr3TtaYW70YCxlIdPyTWBLEtR5ltOz1-phLCOs82wweKCOpL~RvEjTEIaG9wr~CDE2w5hOtF0lHb1IdnsMIcShkiodgTGgFmrf4OD4uoYX9nGrOqvebLfhZ1QmdNgw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4", title: "Minimalis" },
+    { url: "https://s3-alpha-sig.figma.com/img/69dc/f29a/d1619903a8100ba617d7880723e298d7?Expires=1701648000&Signature=VqGfuLFniJeUyvQYozMWlbzrE1eS0LWJOMDbMa4p6xRGPSSR8isAR45y0NJTJgy1lJXs60H1IIB6AN0wmIAHRm1yXQaqKnFbUkQN1i9JfitlYf2zCzpeyUXw29BjeLnuuSHGwNq4jae2ero6HJdBTYarj2WYIycZn3y79pdOahRyQYd5RNpWK2E~0bOKVO61uSorLWQNnItYWAfI2-eg~P1csr3TtaYW70YCxlIdPyTWBLEtR5ltOz1-phLCOs82wweKCOpL~RvEjTEIaG9wr~CDE2w5hOtF0lHb1IdnsMIcShkiodgTGgFmrf4OD4uoYX9nGrOqvebLfhZ1QmdNgw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4", title: "Minimalis" },
+  ];
+  const containerStyle = {
+    width: "500px",
+    height: "280px",
+    margin: "auto",
+  };
   return (
-    <>
-    <NavbarC />
-    <div className="box">
-      <div className="group">
-        <div className="overlap-group">
-          <img className="arrow" alt="Arrow" src="arrow.png" />
-          <div className="rectangle" />
-          <div className="div" />
-          <div className="rectangle-2" />
-          <div className="text-wrapper">Pesan Sekarang !</div>
-          <img className="img" alt="Rectangle" src="rectangle-555.png" />
-          <img className="arrow-2" alt="Arrow" src="image.png" />
-          <img className="arrow-3" alt="Arrow" src="arrow-2.png" />
-          <div className="rectangle-3" />
-          <div className="text-wrapper-2">2 KM</div>
-          <p className="p">10 m x 20 m</p>
-          <div className="text-wrapper-3">Kota Semarang</div>
-          <img className="bed" alt="Bed" src="bed.png" />
-          <img className="size" alt="Size" src="size.png" />
-          <img className="location" alt="Location" src="location.png" />
-        </div>
+    <div>
+        <NavbarC />
+        <div className="box">
+          <div className="retangle">
+            <div className="content" style={containerStyle}>
+              <HeroSlider slides={slides} parentWidth={500} />
+            </div>
+            <div className="content-logo">
+            </div>
+            <Button className='border-0 px-3 py-1 fs-6 rounded-5' style={{background:'#B1907F'}}>Selengkapnya</Button>
+          </div>
       </div>
     </div>
-    </>
-  );
+  )
 };
 
 export default Minimalis
